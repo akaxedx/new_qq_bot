8393b6f1963e54f3f022dc8b65f3310ccdc80270
AKAxedx <1365957941@qq.com>
2025-07-18 16:02:23 +0800

    隔离修复

--- src/main/java/com/akaxedx/newqqbot/plugins/DingPlugin.java
+++ src/main/java/com/akaxedx/newqqbot/plugins/DingPlugin.java
@@ -1,10 +1,9 @@
 package com.akaxedx.newqqbot.plugins;
 
-import com.akaxedx.newqqbot.tools.inter.BaseUtil;
+import com.akaxedx.newqqbot.tools.DingUtil.DingUtilImpl;
 import com.mikuac.shiro.core.Bot;
 import com.mikuac.shiro.core.BotPlugin;
 import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
-import com.mikuac.shiro.dto.event.message.PrivateMessageEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,12 +17,12 @@ public class DingPlugin extends BotPlugin {
     Logger logger = LoggerFactory.getLogger(DingPlugin.class);
 
     @Autowired
-    private BaseUtil baseUtil;
+    private DingUtilImpl dingUtil;
 
     @Override
     public int onGroupMessage(Bot bot, GroupMessageEvent event) {
         String reply = "";
-        reply = baseUtil.doThis(event, bot);
+        reply = dingUtil.doThis(event, bot);
         if (null == reply) {
             return MESSAGE_IGNORE;
         }
---
+++ src/main/java/com/akaxedx/newqqbot/tools/DingUtil/DingUtilImpl.java
@@ -0,0 +1,312 @@
+package com.akaxedx.newqqbot.tools.DingUtil;
+
+import com.akaxedx.newqqbot.entity.Ding;
+import com.akaxedx.newqqbot.mappers.DingMapper;
+import com.akaxedx.newqqbot.tools.BaseReplyConstant;
+import com.akaxedx.newqqbot.tools.inter.BaseUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.dto.action.common.ActionData;
+import com.mikuac.shiro.dto.action.response.StrangerInfoResp;
+import com.mikuac.shiro.dto.event.message.GroupMessageEvent;
+import com.mikuac.shiro.dto.event.message.MessageEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.akaxedx.newqqbot.tools.SenderType;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Random;
+
+@Component
+public class DingUtilImpl implements BaseUtil {
+
+
+    private final Logger logger = LoggerFactory.getLogger(DingUtilImpl.class);
+    @Autowired
+    private DingMapper dingMapper;
+
+
+    @Override
+    public String doThis(MessageEvent event, Bot bot) {
+        String reply;
+        GroupMessageEvent gEvent = (GroupMessageEvent) event;
+        switch (event.getMessage().split("\\[CQ:at,qq=|]")[0]){
+            case DingConstant.DING_GET -> {
+                reply = getDing(gEvent);
+            }
+            case DingConstant.DING_CULTIVATE -> {
+                reply = cultivate(gEvent);
+            }
+            case DingConstant.DING_BATTLE -> {
+                reply = battle(gEvent, bot);
+            }
+            case DingConstant.DING_BANG -> {
+                QueryWrapper<Ding> queryWrapper = new QueryWrapper<>();
+                queryWrapper.lambda()
+                        .eq(Ding::getGroupId, gEvent.getGroupId())
+                        .eq(Ding::getHas,DingConstant.HAS_DING)
+                        .orderBy(true,false,Ding::getLength);
+                List<Ding> dings = dingMapper.selectList(queryWrapper);
+                reply = "金箍榜\n";
+                for (int i = 0; i < dings.size(); i++) {
+                    reply += "第" + (i+1) +"名：" +
+                            bot.getStrangerInfo(dings.get(i).getId(),false).getData().getNickname()+
+                            "，"+dings.get(i).getLength()+"m\n";
+                }
+            }
+            default -> {
+                return null;
+            }
+        }
+        return reply;
+    }
+
+    private String getDing(GroupMessageEvent event) {
+
+
+        String reply = BaseReplyConstant.DING_GET_NULL;
+
+            QueryWrapper<Ding> queryWrapper = new QueryWrapper<>();
+            queryWrapper.lambda().eq(Ding::getId, event.getUserId()).eq(Ding::getGroupId,  event.getGroupId());
+            List<Ding> dings = dingMapper.selectList(queryWrapper);
+            Random r = new Random();
+            Double length = r.nextDouble() * 2 + 3;
+
+            // 没有金箍棒
+            Ding newDing = new Ding(event.getUserId(), length, 1, 0L, event.getGroupId());
+            if (dings.isEmpty()) {
+                dingMapper.insert(newDing);
+                return "[CQ:at,qq="+event.getUserId()+"]" + String.format(reply,
+                        event.getSender().getNickname(),
+                        String.format("%.3f", newDing.getLength()));
+            } else if (dings.get(0).getHas().equals(DingConstant.NO_DING)) {
+                UpdateWrapper<Ding> updateWrapper = new UpdateWrapper<>();
+                updateWrapper.lambda().eq(Ding::getId, newDing.getId()).eq(Ding::getGroupId, newDing.getGroupId());
+                dingMapper.update(newDing,updateWrapper);
+            }
+            // 已有金箍棒
+            reply = "[CQ:at,qq="+event.getUserId()+"]" + String.format(BaseReplyConstant.DING_GET_ONE,
+                    event.getSender().getNickname(),
+                    String.format("%.3f", dings.get(0).getLength()));
+        return reply;
+    }
+    private String cultivate(GroupMessageEvent event) {
+
+        String reply = BaseReplyConstant.NO_DING;
+            QueryWrapper<Ding> queryWrapper = new QueryWrapper<>();
+            queryWrapper.lambda().eq(Ding::getId, event.getUserId()).eq(Ding::getGroupId, event.getGroupId());
+            List<Ding> dings = dingMapper.selectList(queryWrapper);
+
+            Random r = new Random();
+            double luck = r.nextDouble();
+            // 暴击 1-3，普通 0-1，暴击率30%
+            boolean lucky;
+            double length;
+            if (luck >= 0.7) {
+                length =  r.nextDouble() * 2 + 1;
+                lucky = true;
+            } else {
+                length = r.nextDouble();
+                lucky = false;
+            }
+
+            if (dings.isEmpty() || dings.get(0).getHas().equals(DingConstant.NO_DING)) {
+                return "[CQ:at,qq="+event.getUserId()+"]" + String.format(reply,
+                        event.getSender().getNickname());
+            }
+            // 领取了
+            reply = cLength(dings.get(0), length, lucky, event.getSender().getNickname());
+        return reply;
+    }
+    private String battle(GroupMessageEvent event, Bot bot) {
+
+        String reply;
+
+            QueryWrapper<Ding> queryWrapper = new QueryWrapper<>();
+            queryWrapper.lambda().eq(Ding::getId, event.getUserId()).eq(Ding::getGroupId,event.getGroupId());
+            List<Ding> dings = dingMapper.selectList(queryWrapper);
+
+            Random r = new Random();
+            double luck = r.nextDouble();
+            // 暴击 1-3，普通 0-1，暴击率30%
+            boolean alive;
+            boolean lucky;
+            double length;
+            length =  r.nextDouble() * 2 + 1;
+            if (luck > 0.02) {
+                alive = true;
+                lucky = luck > 0.51;
+            } else {
+                alive = false;
+                lucky = luck > 0.01;
+            }
+
+            // 没领或清空
+            if (dings.isEmpty() || dings.get(0).getHas().equals(DingConstant.NO_DING)) {
+                return String.format(BaseReplyConstant.NO_DING,
+                        event.getSender().getNickname());
+            }
+            logger.info("消息内容->{}", event.getMessage());
+            Ding thisUser = dings.get(0);
+            String[] split = event.getMessage().split("\\[CQ:at,qq=|]");
+            if (split.length == 1) {
+                // 无指定对象
+                QueryWrapper<Ding> queryWrapper1 = new QueryWrapper<>();
+                queryWrapper1.lambda().eq(Ding::getHas, DingConstant.HAS_DING).eq(Ding::getGroupId, event.getGroupId());
+                List<Ding> allDingList = dingMapper
+                        .selectList(queryWrapper1)
+                        .stream()
+                        .filter(ding -> !ding.getId().equals(thisUser.getId()))
+                        .toList();
+                if (allDingList.isEmpty()) {
+                    reply = "[CQ:at,qq="+event.getUserId()+"]"+"无敌是多么寂寞，无人有金箍棒与您一战";
+                } else {
+                    int battleUserIndex = r.nextInt(allDingList.size());
+                    Ding battleUser = allDingList.get(battleUserIndex);
+                    ActionData<StrangerInfoResp> strangerInfo = bot.getStrangerInfo(battleUser.getId(), false);
+                    String battleName = strangerInfo.getData().getNickname();
+                    reply = bLength(event.getSender().getNickname(), battleName, lucky, alive, thisUser, length, battleUser, bot, event.getGroupId());
+                }
+            } else {
+                logger.info("进入分流");
+                QueryWrapper<Ding> queryWrapper1 = new QueryWrapper<>();
+                queryWrapper1.lambda().eq(Ding::getId, split[1]).eq(Ding::getGroupId, event.getGroupId());
+                List<Ding> dings1 = dingMapper.selectList(queryWrapper1);
+                if (dings1.isEmpty() || dings1.get(0).getHas().equals(DingConstant.NO_DING)) {
+                    reply = BaseReplyConstant.THAT_NO_DING;
+                } else {
+                    Ding battleUser = dings1.get(0);
+                    ActionData<StrangerInfoResp> strangerInfo2 = bot.getStrangerInfo(battleUser.getId(), false);
+                    String battleName = strangerInfo2.getData().getNickname();
+                    reply = bLength(event.getSender().getNickname(), battleName, lucky, alive, thisUser, length, battleUser, bot, event.getGroupId());
+                }
+            }
+
+        return reply;
+    }
+    private String cLength(Ding ding, Double length, Boolean lucky, String userName) {
+        long now = Instant.now().getEpochSecond();
+        Long lastest = ding.getTime();
+        long middle = now - lastest;
+        // 长度超长
+        if (ding.getLength() >= 20) {
+            return "[CQ:at,qq="+ding.getId()+"]"+BaseReplyConstant.TOO_LONG;
+        }
+        // 时间不够
+        if (middle < 180) {
+            return "[CQ:at,qq="+ding.getId()+"]"+ String.format(BaseReplyConstant.NEED_RELAX, 180-middle);
+        }
+        // 时间够了
+        String reply;
+        Double newLength = ding.getLength() + length;
+        ding.setLength(newLength);
+        ding.setTime(now);
+        UpdateWrapper<Ding> updateWrapper = new UpdateWrapper<>();
+        updateWrapper
+                .lambda()
+                .eq(Ding::getId,ding.getId())
+                .eq(Ding::getGroupId,ding.getGroupId());
+        dingMapper.update(ding,updateWrapper);
+        // 暴击了
+        if (lucky) {
+            reply = String.format(BaseReplyConstant.CULTIVATE,
+                    "绝佳",
+                    String.format("%.3f", length) ,
+                    String.format("%.3f", ding.getLength()));
+        } else {
+            reply = String.format(BaseReplyConstant.CULTIVATE,
+                    "一般",
+                    String.format("%.3f", length)  ,
+                    String.format("%.3f", ding.getLength()));
+        }
+        return "[CQ:at,qq="+ding.getId()+"]"+ reply;
+    }
+    private String bLength(String thisName,String battleName, boolean lucky, boolean alive, Ding thisUser, double length, Ding battleUser,Bot bot,Long groupId) {
+        String reply = null;
+        double battleLength = 0;
+        double thisLength = 0;
+        // 已有金箍棒
+        UpdateWrapper<Ding> updateWrapperB = new UpdateWrapper<>();
+        UpdateWrapper<Ding> updateWrapperT = new UpdateWrapper<>();
+        updateWrapperB
+                .lambda()
+                .eq(Ding::getId,battleUser.getId())
+                .eq(Ding::getGroupId,battleUser.getGroupId());
+        updateWrapperT
+                .lambda()
+                .eq(Ding::getId,thisUser.getId())
+                .eq(Ding::getGroupId,thisUser.getGroupId());
+        if (lucky && alive) {
+            battleLength = battleUser.getLength() - length;
+            thisLength = thisUser.getLength() + length;
+            battleUser.setLength(battleLength);
+            thisUser.setLength(thisLength);
+            dingMapper.update(battleUser,updateWrapperB);
+            dingMapper.update(thisUser,updateWrapperT);
+            reply = String.format(BaseReplyConstant.BATTLE_SUCCESS,
+                    battleName,
+                    String.format("%.3f", thisLength),
+                    battleName,
+                    String.format("%.3f", battleLength));
+        } else if (lucky && !alive) {
+            thisLength = thisUser.getLength() + 2 * length;
+            battleUser.setHas(DingConstant.NO_DING);
+            thisUser.setLength(thisLength);
+            dingMapper.update(battleUser,updateWrapperB);
+            dingMapper.update(thisUser,updateWrapperT);
+            reply = String.format(BaseReplyConstant.BATTLE_BOOM_THAT,
+                    battleName,
+                    battleName,
+                    String.format("%.3f",thisLength),
+                    battleName);
+        } else if (!lucky && alive) {
+            battleLength = battleUser.getLength() + length;
+            thisLength = thisUser.getLength() - length;
+            battleUser.setLength(battleLength);
+            thisUser.setLength(thisLength);
+            dingMapper.update(battleUser,updateWrapperB);
+            dingMapper.update(thisUser,updateWrapperT);
+            reply = String.format(BaseReplyConstant.BATTLE_FAIL,
+                    battleName,
+                    String.format("%.3f", thisLength),
+                    battleName,
+                    String.format("%.3f", battleLength));
+        } else if (!lucky && !alive) {
+            battleLength = battleUser.getLength() + 2 * length;
+            battleUser.setLength(battleLength);
+            thisUser.setHas(DingConstant.NO_DING);
+            dingMapper.update(battleUser,updateWrapperB);
+            dingMapper.update(thisUser,updateWrapperT);
+            reply = String.format(BaseReplyConstant.BATTLE_BOOM_YOU,
+                    battleName,
+                    battleName,
+                    String.format("%.3f",battleLength));
+        }
+
+        reply = "[CQ:at,qq="+thisUser.getId()+"]"+reply;
+
+        if (battleLength >= 30) {
+            UpdateWrapper<Ding> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.lambda().eq(Ding::getGroupId, groupId).set(Ding::getHas,0L);
+            dingMapper.update(null, updateWrapper);
+            bot.sendGroupMsg(groupId,reply,false);
+            reply = String.format(BaseReplyConstant.BATTLE_WIN,
+                    battleName,
+                    battleLength);
+        }
+        if (thisLength >= 30) {
+            UpdateWrapper<Ding> updateWrapper = new UpdateWrapper<>();
+            updateWrapper.lambda().eq(Ding::getGroupId, groupId).set(Ding::getHas,0L);
+            dingMapper.update(null, updateWrapper);
+            bot.sendGroupMsg(groupId,reply,false);
+            reply = String.format(BaseReplyConstant.BATTLE_WIN,
+                    thisName,
+                    thisName);
+        }
+        return reply;
+    }
+}
